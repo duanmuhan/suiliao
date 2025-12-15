9a64949e1d54023e7392a0346bed55f69fe1f11a
hanmuling <8605346+duanmuhan@users.noreply.github.com>
2025-12-15 08:47:33 +0800

    提交主页面代码

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from 'react-native';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Layout,
+} from '@ui-kitten/components';
+import React from 'react';
+import AgeScreen from './AgeScreen.tsx';
+import { LabelScreen } from './LabelScreen.tsx';
+import { NameScreen } from './NameScreen.tsx';
 
 export const HomeScreen = () => {
+
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const renderContent = () => {
+    switch (selectedIndex) {
+      case 0:
+        return <AgeScreen />
+      case 1:
+        return <LabelScreen />
+      case 2:
+        return <NameScreen />
+      default:
+        return null
+    }
+  }
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <Layout style={{ flex: 1 }}>
+      <Layout style={{ flex: 1 }}>
+        {renderContent()}
+      </Layout>
+      <BottomNavigation
+        selectedIndex={selectedIndex}
+        onSelect={index => setSelectedIndex(index)}
+      >
+        <BottomNavigationTab title="Home" />
+        <BottomNavigationTab title="Profile" />
+        <BottomNavigationTab title="Age" />
+      </BottomNavigation>
+    </Layout>
   )
 }
